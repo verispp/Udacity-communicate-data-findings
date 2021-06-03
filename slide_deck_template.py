aaf344dc8f61b14769bfd42a3119d5511c2fa32f
Veris <78966878+verispp@users.noreply.github.com>
2021-06-03 11:41:08 +0200

    Add files via upload

---
+++ slide_deck_template.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # (Presentation Title)
+# ## by (your name here)
+
+# ## Investigation Overview
+# 
+# > Describe the overall goals of your presentation here.
+# 
+# ## Dataset Overview
+# 
+# > Provide a brief overview of the dataset to be presented here.
+
+# In[ ]:
+
+
+# import all packages and set plots to be embedded inline
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# suppress warnings from final output
+import warnings
+warnings.simplefilter("ignore")
+
+
+# In[ ]:
+
+
+# load in the dataset into a pandas dataframe
+
+
+# > Note that the above cells have been set as "Skip"-type slides. That means
+# that when the notebook is rendered as http slides, those cells won't show up.
+
+# ## (Visualization 1)
+# 
+# > Write a comment about your visualization here. The visualization should be in
+# the next cell, as a sub-slide type. Make sure your visualizations are polished!
+
+# In[ ]:
+
+
+
+
+
+# ## (Visualization 2)
+# 
+# > You should have at least three visualizations in your presentation,
+# but feel free to add more if you'd like!
+
+# In[ ]:
+
+
+
+
+
+# ## (Visualization 3)
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# > Once you're ready to finish your presentation, check your output by using
+# nbconvert to export the notebook and set up a server for the slides. From the
+# terminal or command line, use the following expression:
+# > > `jupyter nbconvert <file_name>.ipynb --to slides --post serve --template output_toggle`
+# 
+# > This should open a tab in your web browser where you can scroll through your
+# presentation. Sub-slides can be accessed by pressing 'down' when viewing its parent
+# slide. Make sure you remove all of the quote-formatted guide notes like this one
+# before you finish your presentation!
+
+# In[ ]:
+
+
+
+
